70f6f6ed0de5d347e92def4ec1d99b9a3eeaf0e9
Yuriy Syrovetskiy <cblp@cblp.su>
2019-09-29 23:10:25 +0300

    fixup! fixup! Switch ff-qt to Qtah

--- ff-qt/Main.hs
+++ ff-qt/Main.hs
@@ -6,12 +6,11 @@
 {-# LANGUAGE ViewPatterns #-}
 
 module Main
-  ( main
-    )
+  ( main,
+  )
 where
 
 import Control.Concurrent (forkIO)
-import Cpp (MainWindow, ffCtx, includeDependent)
 import Data.Foldable (for_)
 import Data.Maybe (fromJust, fromMaybe, isJust)
 import qualified Data.Text as Text
@@ -23,32 +22,28 @@ import FF
   ( fromRgaM,
     getDataDir,
     loadTasks,
-    noDataDirectoryMessage
-    )
+    noDataDirectoryMessage,
+  )
 import FF.Config (loadConfig)
 import FF.Types
-  ( Entity (Entity),
-    Note (Note),
+  ( Entity (Entity, entityId, entityVal),
+    Note (Note, note_end, note_start, note_status, note_text, note_track),
     NoteId,
     NoteStatus (TaskStatus),
-    NoteView(note),
+    NoteView (note),
     Status (Active),
-    entityId,
-    entityVal,
+    Track (track_externalId, track_provider, track_source, track_url),
     loadNote,
-    note_end,
-    note_start,
-    note_status,
-    note_text,
-    note_track,
-    track_externalId,
-    track_provider,
-    track_source,
-    track_url
-    )
+  )
 import Foreign (Ptr)
 import Foreign.C (CInt)
+import Foreign.Hoppy.Runtime (withScopedPtr)
 import Foreign.StablePtr (newStablePtr)
+import qualified Graphics.UI.Qtah.Core.QCoreApplication as QCoreApplication
+import qualified Graphics.UI.Qtah.Widgets.QApplication as QApplication
+import Graphics.UI.Qtah.Widgets.QMainWindow (QMainWindow)
+import qualified Graphics.UI.Qtah.Widgets.QMainWindow as QMainWindow
+import qualified Graphics.UI.Qtah.Widgets.QWidget as QWidget
 import qualified Language.C.Inline.Cpp as Cpp
 import Paths_ff_qt (version)
 import RON.Storage.Backend (DocId (DocId))
@@ -56,16 +51,10 @@ import qualified RON.Storage.FS as Storage
 import RON.Storage.FS
   ( CollectionDocId (CollectionDocId),
     runStorage,
-    subscribeForever
-    )
+    subscribeForever,
+  )
 
-Cpp.context $ Cpp.cppCtx <> Cpp.bsCtx <> ffCtx
-
-Cpp.include "<QtWidgets/QApplication>"
-
-includeDependent "FFI/Cxx.hxx"
-
-includeDependent "MainWindow.hxx"
+data UI = UI {window :: QMainWindow}
 
 main :: IO ()
 main = do
@@ -73,32 +62,34 @@ main = do
   path <- getDataDirOrFail
   storage <- Storage.newHandle path
   storagePtr <- newStablePtr storage
-  -- set up UI
-  mainWindow <-
-    [Cpp.block| MainWindow * {
-      int argc = 0;
-      char argv0[] = "ff-qt";
-      char * argv[] = {argv0, NULL};
+  withApp $ \app -> do
+    ui@UI {window} <- setupUI
+    --   int argc = 0;
+    --   char argv0[] = "ff-qt";
+    --   char * argv[] = {argv0, NULL};
 
-      auto app = new QApplication(argc, argv);
-      app->setOrganizationDomain("ff.cblp.su");
-      app->setOrganizationName("ff");
-      app->setApplicationName("ff");
-      app->setApplicationVersion(QString::fromStdString($bs-cstr:version'));
+    --   auto app = new QApplication(argc, argv);
+    --   app->setOrganizationDomain("ff.cblp.su");
+    --   app->setOrganizationName("ff");
+    --   app->setApplicationName("ff");
+    --   app->setApplicationVersion(QString::fromStdString($bs-cstr:version'));
+    QWidget.show window
+    -- load current data to the view, asynchronously
+    _ <-
+      forkIO $ do
+        activeTasks <- runStorage storage (loadTasks False)
+        for_ activeTasks $ upsertTask ui . note
+    -- update the view with future changes
+    _ <- forkIO $ subscribeForever storage $ upsertDocument storage ui
+    -- run UI
+    QCoreApplication.exec
 
-      auto window = new MainWindow($(StorageHandle storagePtr));
-      window->show();
-      return window;
-    } |]
-  -- load current data to the view, asynchronously
-  _ <-
-    forkIO $ do
-      activeTasks <- runStorage storage (loadTasks False)
-      for_ activeTasks $ upsertTask mainWindow . note
-  -- update the view with future changes
-  _ <- forkIO $ subscribeForever storage $ upsertDocument storage mainWindow
-  -- run UI
-  [Cpp.block| void { qApp->exec(); } |]
+withApp = withScopedPtr $ do
+  args <- getArgs
+  QApplication.new args
+
+setupUI = do
+  pure UI {}
 
 getDataDirOrFail :: IO FilePath
 getDataDirOrFail = do
@@ -108,14 +99,14 @@ getDataDirOrFail = do
     Nothing -> fail noDataDirectoryMessage
     Just path -> pure path
 
-upsertDocument :: Storage.Handle -> Ptr MainWindow -> CollectionDocId -> IO ()
+upsertDocument :: Storage.Handle -> UI -> CollectionDocId -> IO ()
 upsertDocument storage mainWindow (CollectionDocId docid) = case docid of
   (cast -> Just (noteId :: NoteId)) -> do
     note <- runStorage storage $ loadNote noteId
     upsertTask mainWindow note
   _ -> pure ()
 
-upsertTask :: Ptr MainWindow -> Entity Note -> IO ()
+upsertTask :: UI -> Entity Note -> IO ()
 upsertTask mainWindow Entity {entityId = DocId nid, entityVal = note} = do
   let nid' = encodeUtf8 $ Text.pack nid
       isActive = note_status == Just (TaskStatus Active)
@@ -125,26 +116,27 @@ upsertTask mainWindow Entity {entityId = DocId nid, entityVal = note} = do
       (startYear, startMonth, startDay) = toGregorianC $ fromJust note_start
       (endYear, endMonth, endDay) = maybe (0, 0, 0) toGregorianC note_end
       isTracking = isJust note_track
-      provider   = encodeUtf8 $ fromMaybe "" $ note_track >>= track_provider
-      source     = encodeUtf8 $ fromMaybe "" $ note_track >>= track_source
+      provider = encodeUtf8 $ fromMaybe "" $ note_track >>= track_provider
+      source = encodeUtf8 $ fromMaybe "" $ note_track >>= track_source
       externalId = encodeUtf8 $ fromMaybe "" $ note_track >>= track_externalId
-      url        = encodeUtf8 $ fromMaybe "" $ note_track >>= track_url
-  [Cpp.block| void {
-    $(MainWindow * mainWindow)->upsertTask({
-      .id = $bs-cstr:nid',
-      .isActive = $(bool isActive),
-      .text = $bs-cstr:text,
-      .start = {$(int startYear), $(int startMonth), $(int startDay)},
-      .end   = {$(int   endYear), $(int   endMonth), $(int   endDay)},
-      .isTracking = $(bool isTracking),
-      .track = {
-        .provider   = $bs-cstr:provider,
-        .source     = $bs-cstr:source,
-        .externalId = $bs-cstr:externalId,
-        .url        = $bs-cstr:url,
-      },
-    });
-  } |]
+      url = encodeUtf8 $ fromMaybe "" $ note_track >>= track_url
+  -- [Cpp.block| void {
+  --   $(MainWindow * mainWindow)->upsertTask({
+  --     .id = $bs-cstr:nid',
+  --     .isActive = $(bool isActive),
+  --     .text = $bs-cstr:text,
+  --     .start = {$(int startYear), $(int startMonth), $(int startDay)},
+  --     .end   = {$(int   endYear), $(int   endMonth), $(int   endDay)},
+  --     .isTracking = $(bool isTracking),
+  --     .track = {
+  --       .provider   = $bs-cstr:provider,
+  --       .source     = $bs-cstr:source,
+  --       .externalId = $bs-cstr:externalId,
+  --       .url        = $bs-cstr:url,
+  --     },
+  --   });
+  -- } |]
+  _
 
 toGregorianC :: Day -> (CInt, CInt, CInt)
 toGregorianC day = (y, m, d)
